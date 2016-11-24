a4f4f1ee15c41d85973eeedd69f3d4e5a95b39b4
matthewglassman <matthewglassmn78@gmail.com>
2016-11-23 21:33:05 -0500

    Using some of the templates provided with homework. Added image for background

---
+++ assets/javascript/logic.js
@@ -0,0 +1,18 @@
+// GLOBAL VARIABLES (Accessible by all functions)
+// ==================================================================================================
+var bwayPhoto
+var bwayTitle = 
+var usersGuess = [];
+var guessesAvailable = 
+var guessesLeft = guessesAvailable - 1;
+var wins = 0;
+var start = "";
+var lettersguessed = [];
+
+
+// FUNCTIONS (These are bits of code that we will call upon to run when needed)
+// ==================================================================================================
+
+
+// MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
+// ==================================================================================================
