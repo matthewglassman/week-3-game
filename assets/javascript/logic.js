bdf1fb34f31391e1976f31c90c245d7b07fc0045
matthewglassman <matthewglassmn78@gmail.com>
2016-11-27 16:54:16 -0500

    Adding functions.. and praying

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,21 +1,20 @@
 // GLOBAL VARIABLES (Accessible by all functions)
 // ==================================================================================================
-var bwayPhoto
-var bwayTitle = ["phantom", "cats", "hairspray", "hamilton"]
-var usersGuess = [];
-var guessesAvailable = 
-var guessesLeft = guessesAvailable - 1;
+// var bwayPhoto  may hold a photo for related bway show
+var wordBank = ["PHANTOM", "CATS", "HAIRSPRAY", "HAMILTON", "RENT", "GREASE"]
+var chosenWord = ""; //will hold the word that is randomly chosen. Produced via function.
+var lettersGuessed = []; //an array filled by keystrokes entered by the user.  Uses a .push or .appendChild?
+var guessesRemaining = chosenWord.length + 4
 var wins = 0;
-var start = "";
-var lettersguessed = [];
-
+var losses = 0;
+var blanks = ["_"]; //will replace the letters in the chosen word once it is split into an array of the letters it contains
+var gameOver = 
 
 // FUNCTIONS (These are bits of code that we will call upon to run when needed)
 // ==================================================================================================
-document.onkeyup = function(){
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	console.log(userguess);
-}
+//Chose word for user to guess.
+$(document).ready(function() {
+	chosenWord = Math.floor(Math.random() * wordBank.length);
 
 // MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
 // ==================================================================================================
